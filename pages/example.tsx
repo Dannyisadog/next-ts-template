22c609c7e589c9f58b767852d8baf21e9093e665
Danny <dannyisadog10@gmail.com>
2022-06-18 15:34:17 +0800

    feat: example page

---
+++ pages/example.tsx
@@ -0,0 +1,31 @@
+import { NextPage } from 'next';
+import ExampleProvider from 'providers/example/Provider';
+import Example from 'views/desktop/Example';
+import { ContextProperty } from 'providers/shared/constants';
+
+interface Props {
+  data: ContextProperty;
+}
+
+const ExamplePage: NextPage<Props> = (props) => {
+	return (
+		<ExampleProvider data={{ ...props }}>
+			<Example />
+		</ExampleProvider>
+	);
+};
+
+export default ExamplePage;
+
+export async function getServerSideProps() {
+	return {
+		props: {
+			data: {
+				title: 'Example',
+				description: 'Example',
+				url: 'https://example.com',
+				image: 'https://example.com/image.jpg',
+			},
+		},
+	};
+}
